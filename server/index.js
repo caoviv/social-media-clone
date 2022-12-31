3380cfff1133497589183e1709705c0c9302b884
Vivian C <viviancao58@gmail.com>
2022-12-31 22:32:07 +0000

    setup mongoose

--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// configurations
+/* configurations */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.CrossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// file storage - as per multer github
+/* file storage - as per multer github */
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
@@ -34,3 +34,19 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* mongoose setup */
+// set port and use 6001 as backup
+const PORT = process.env.PORT || 6001;
+
+mongoose.set("strictQuery", false);
+mongoose
+.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    //confirm server port
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
