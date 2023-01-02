b77c64454218f95e228e0f0e6181bcd53484cd57
Vivian C <viviancao58@gmail.com>
2023-01-02 14:52:06 +0100

    create User model

---
+++ server/models/User.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 2 /*minimum two characters*/,
+      max: 50 /*maximum fifty characters*/,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2 /*minimum two characters*/,
+      max: 50 /*maximum fifty characters*/,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50 /*maximum fifty characters*/,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 5,
+    },
+    password: {
+      type: String,
+      default: "",
+    },
+    friends: {
+      type: Array,
+      default: [],
+    },
+    location: String,
+    occupation: String,
+    viewedProfile: Number,
+    impressions: Number,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User
