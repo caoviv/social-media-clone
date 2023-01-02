96637a9f84b527c3c50fa558a5d70b02026d183c
Vivian C <viviancao58@gmail.com>
2023-01-02 14:51:48 +0100

    add routes with files

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 /* configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -34,6 +35,10 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* routes with files */
+app.post("/auth/register", upload.single("picture"), register);
+
 
 /* mongoose setup */
 // set port and use 6001 as backup
