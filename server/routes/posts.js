6d4fe86ede129a844621d1c32e4c96e3745b0c9e
Vivian C <viviancao58@gmail.com>
2023-01-04 22:32:59 +0100

    create post routes

---
+++ server/routes/posts.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router()
+
+/* read */
+// gets users feed - all post, there is no curated feed algorithm 
+router.get("/", verifyToken, getFeedPosts);
+// gets all posts made by a specific user via id
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* update */
+// like/unlike a post 
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
