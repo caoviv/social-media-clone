94e959146c534f02db4dfeffb647be74fb26457e
Vivian C <viviancao58@gmail.com>
2023-01-11 20:22:41 +0100

    fix typo to fix router.patch 404 error

--- server/routes/users.js
+++ server/routes/users.js
@@ -13,6 +13,6 @@ router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
 
 /* update routes */
-router.patch("/:id/:friendID", verifyToken, addRemoveFriend);
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
 export default router;
